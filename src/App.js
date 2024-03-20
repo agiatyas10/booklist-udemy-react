49a0f7973cbe2e5d9350ea1503b8416c3bf95274
agiatyas10 <agiatyas10@gmail.com>
2024-03-20 17:21:44 +0700

    integrate to local API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,62 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
 
-function App(props) {
+function App() {
     const [books, setBooks] = useState([])
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, [])
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        })
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle };
+                return { ...book, ...response.data };
             }
             return book;
         });
         setBooks(updatedBooks);
+
+        console.log(response)
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        axios.delete(`http://localhost:3001/books/${id}`)
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
 
         setBooks(updatedBooks)
     }
-    const createBook = (title) => {
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+
+        console.log(response);
         const updatedBooks = [
-            ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title
-            },
+            ...books, response.data
         ]
         setBooks(updatedBooks)
+        // const updatedBooks = [
+        //     ...books,
+        //     {
+        //         id: Math.round(Math.random() * 9999),
+        //         title
+        //     },
+        // ]
+        // setBooks(updatedBooks)
     }
     return (
         <div className='app'>
